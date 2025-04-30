0727fa20cb2861fd3a9ed2b39985464f41019154
Cheesenose <40203166+Cheesenose@users.noreply.github.com>
2025-04-30 22:49:33 +1000

    Rearranging the Files and cleaning new data
    Rearrange files to be a bit cleaner and also decentralise the coding so it isnt literally all in one notebook.
    New folder for us to do all our stuff for the report due end of this week

--- data-combing/H5_data_combing.py
+++ data-combing/H5_data_combing.py
@@ -1,1 +1,10 @@
+import pandas as pd
 
+df_harvest = pd.read_csv('h5_i2_2019-2022_hand_harvest.csv')
+df_weather = pd.read_csv('h5-i2_2016-2021_daily-weather.csv')
+print("Columns in df_harvest:", df_harvest.columns)
+
+print(f"Before consolidating to years with weather data: {len(df_harvest)}")
+print(f"After consolidating to years with weather data: {len(df_harvest)}")
+
+#df_harvest.to_csv('./datasets/h5-i2_2016-2021_daily_harvest_cleaned.csv', index=False)
