58aad81dfc69172ec59b4d13684ca53ff68c0a5a
Cheesenose <40203166+Cheesenose@users.noreply.github.com>
2025-05-17 14:51:54 +1000

    Heatmap and Histogram Generation

--- data-combing/GIS/Bandnames.py
+++ data-combing/GIS/Bandnames.py
@@ -4,7 +4,13 @@ ee.Authenticate()
 ee.Initialize()
 print(ee.String('Hello from the Earth Engine servers!').getInfo())
 
-evapotranspiration_dataset = ee.ImageCollection('TERN/AET/CMRSET_LANDSAT_V2_2').filterDate('2023-01-01', '2023-01-02').mean().select('ETa')
+import os
+import pandas as pd
+from pathlib import Path
+from datetime import datetime, timedelta
 
-print(evapotranspiration_dataset.bandNames().getInfo())
-
+# Path to the produced folder
+produced_folder = Path(__file__).parent / "produced" / "Reduced"
+output_folder = Path(__file__).parent / "allocated-weather"
+output_folder.mkdir(exist_ok=True)  # Ensure the folder exists
+first = True
